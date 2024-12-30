7cdda93dbe92c580b0db7e665bf10d5c9f65ced6
samuel0953 <113757453+samuel0953@users.noreply.github.com>
2024-12-30 12:23:25 +0300

    Unused data are removed to minify the app

--- constants/index.ts
+++ constants/index.ts
@@ -6,24 +6,7 @@ type Catagory = {
   name: string;
   emoji: string;
 };
-export const CATAGORIES: Catagory[] = [
-  {
-    name: "All",
-    emoji: "😋",
-  },
-  {
-    name: "Pizza",
-    emoji: "🍕",
-  },
-  {
-    name: "Burger",
-    emoji: "🍔",
-  },
-  {
-    name: "Drinks",
-    emoji: "🍸",
-  },
-];
+export const CATAGORIES: Catagory[] = [];
 
 export const TABS = {
   meals: "Meals",
